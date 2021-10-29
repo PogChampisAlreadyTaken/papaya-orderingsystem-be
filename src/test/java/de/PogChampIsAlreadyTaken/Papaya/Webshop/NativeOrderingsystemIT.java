314987ba895a71bb02f66b75ad63ee8ccfe36738
studlintmo6711 <moritz.lintterer@stud.th-rosenheim.de>
2021-10-29 13:59:08 +0200

    Revert "Initial Project Commit"
    This reverts commit 58caba1f8c39d2889169aaf90102f9abb779f09f.

--- src/test/java/de/PogChampIsAlreadyTaken/Papaya/Webshop/NativeOrderingsystemIT.java
+++
@@ -1,9 +0,0 @@
-package de.PogChampIsAlreadyTaken.Papaya.Webshop;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeOrderingsystemIT extends OrderingsystemTest {
-
-    // Execute the same tests but in native mode.
-}
