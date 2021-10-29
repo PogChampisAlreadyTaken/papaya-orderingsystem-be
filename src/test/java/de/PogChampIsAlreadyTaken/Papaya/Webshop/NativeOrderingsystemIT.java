96b3e1571adca7f9669613adba08f3c822494cd6
Dac Bao Long Ho <baolong.ho10@gmail.com>
2021-10-29 14:13:54 +0200

    initial quarkus setup

---
+++ src/test/java/de/PogChampIsAlreadyTaken/Papaya/Webshop/NativeOrderingsystemIT.java
@@ -0,0 +1,9 @@
+package de.PogChampIsAlreadyTaken.Papaya.Webshop;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeOrderingsystemIT extends OrderingsystemTest {
+
+    // Execute the same tests but in native mode.
+}
