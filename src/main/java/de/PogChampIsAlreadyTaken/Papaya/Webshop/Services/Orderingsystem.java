d0225a4cdf349cf42e5c2dba88cf1103225f2826
DaccoTheTaco <baolong.ho10@gmail.com>
2021-12-09 19:33:31 +0100

    Fixed hello method in Orderingsystem for test and frontend implementation

--- src/main/java/de/PogChampIsAlreadyTaken/Papaya/Webshop/Services/Orderingsystem.java
+++ src/main/java/de/PogChampIsAlreadyTaken/Papaya/Webshop/Services/Orderingsystem.java
@@ -2,6 +2,8 @@ package de.PogChampIsAlreadyTaken.Papaya.Webshop.Services;
 
 import de.PogChampIsAlreadyTaken.Papaya.Webshop.Baseclasses.Address;
 
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -11,15 +13,17 @@ import javax.ws.rs.core.Response;
 public class Orderingsystem {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
     public String hello() {
-        return "Hello Orderingsystem, QwQ";
+        String hello = "Hello Orderingsystem, QwQ";
+        Jsonb jsonb = JsonbBuilder.create();
+        return jsonb.toJson(hello);
     }
 
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getAddress(@PathParam("id")long id){
+    public Response getAddress(@PathParam("id") long id) {
         Address address = Address.findById(id);
         return Response.ok().entity(address).build();
     }
@@ -27,7 +31,7 @@ public class Orderingsystem {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Transactional
-    public Response postAddress(Address address){
+    public Response postAddress(Address address) {
         address.persist();
         return Response.ok().entity(address).build();
     }
