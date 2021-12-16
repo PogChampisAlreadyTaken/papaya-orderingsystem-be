263f0e2e694308adcd20e618b94075d970089972
fhesselfeld <franziska.hesselfeld@extern.mmi-media.de>
2021-12-16 14:58:25 +0100

    First draft of implement Get REST call for freeTables

--- src/main/java/de/PogChampIsAlreadyTaken/Papaya/Webshop/Baseclasses/Reservation/Reservation.java
+++ src/main/java/de/PogChampIsAlreadyTaken/Papaya/Webshop/Baseclasses/Reservation/Reservation.java
@@ -4,6 +4,7 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 import javax.persistence.Entity;
 import java.sql.Date;
+import java.sql.Time;
 
 @Entity
 public class Reservation extends PanacheEntity {
@@ -11,5 +12,6 @@ public class Reservation extends PanacheEntity {
     public int tableid;
     public String name;
     public int phonenumber;
-    public Date time;
+    public Date reservationdate;
+    public Time reservationtime;
 }
--- src/main/java/de/PogChampIsAlreadyTaken/Papaya/Webshop/Services/Reservation/Reservationsystem.java
+++ src/main/java/de/PogChampIsAlreadyTaken/Papaya/Webshop/Services/Reservation/Reservationsystem.java
@@ -3,11 +3,20 @@ package de.PogChampIsAlreadyTaken.Papaya.Webshop.Services.Reservation;
 import de.PogChampIsAlreadyTaken.Papaya.Webshop.Baseclasses.Reservation.Reservation;
 import de.PogChampIsAlreadyTaken.Papaya.Webshop.Baseclasses.Reservation.RestaurantTable;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 @Path("/reservation")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -27,9 +36,63 @@ public class Reservationsystem {
     @POST
     @Transactional
     public Response postReservation(Reservation reservation) {
+        //buissness logic
+        //only persist, if
+        /*
+        Reservierung:
+
+Tisch ist für 2 h reserviert.
+
+Möchte man reservieren, so gibt man den Start-Zeitpunkt ein.
+
+Die Startzeit wird genommen und es wird überprüft:
+
+Gibt es bereits den Tisch schon mit dieser Startzeit?
+Liegt der Tisch in einer 2h-Sperre?
+Wenn nicht --> Reservierung erfolgreich
+Sonst: Fehlerfall --> Tisch kann nicht reserviert werden, andere Zeit wählen
+2 unterschiedliche Restcalls:
+POST: CheckAvailability
+POSt: Reservation
+         */
         reservation.persist();
         return Response.ok().entity(reservation).build();
     }
+    
+    @GET
+    public Response getFreeTables(Date reservationDateWanted, Time reservationTimeWanted){
+        //get all free tables at a specific time
+        // get all Reservation on this day:
+        List<Reservation> reservatedTablesAtSpecificDay = Reservation.list("reservationdate", reservationDateWanted);
+        List<Reservation> alreadyTaken = new ArrayList<Reservation>();
+
+        //for checking free table
+        LocalTime reservationTimeWantedLoc = reservationTimeWanted.toLocalTime();
+        int duration = LocalTime.of(2,0).getHour();
+        LocalTime reservationTimeWantedMin = reservationTimeWantedLoc.plusHours(duration);
+        LocalTime reservationTimeWantedMax = reservationTimeWantedLoc.minusHours(duration);
+
+        //check if a table is reservated at the time
+        reservatedTablesAtSpecificDay.forEach(reservation -> {
+            if(reservationTimeWantedLoc.compareTo(reservationTimeWanted.toLocalTime()) == 0){
+                alreadyTaken.add(reservation);
+            }
+            if(reservation.reservationtime.toLocalTime().compareTo(reservationTimeWantedMax) < 0 &&
+                reservation.reservationtime.toLocalTime().compareTo(reservationTimeWantedMin) > 0){
+                //table is already tanken
+                alreadyTaken.add(reservation);
+            }
+        });
+
+        List<RestaurantTable> restaurantTable = RestaurantTable.listAll();
+        
+
+        //find all reservations, which will be in the futuret
+        List<RestaurantTable> freeTables = new ArrayList<>();
+        return Response.ok().entity(freeTables).build();
+    }
+
+
 
     //Delete a table
     @DELETE
@@ -44,4 +107,6 @@ public class Reservationsystem {
         table.delete();
         return Response.ok().entity(table).build();
     }
+
+
 }
