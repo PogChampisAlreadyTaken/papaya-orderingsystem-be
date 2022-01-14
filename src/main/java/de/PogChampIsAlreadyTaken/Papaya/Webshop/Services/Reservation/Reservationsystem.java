3e552c9f2d192d138b0809ab629855d5e52eb935
fhesselfeld <franziska.hesselfeld@extern.mmi-media.de>
2022-01-14 12:45:20 +0100

    WIP

--- src/main/java/de/PogChampIsAlreadyTaken/Papaya/Webshop/Services/Reservation/Reservationsystem.java
+++ src/main/java/de/PogChampIsAlreadyTaken/Papaya/Webshop/Services/Reservation/Reservationsystem.java
@@ -2,12 +2,14 @@ package de.PogChampIsAlreadyTaken.Papaya.Webshop.Services.Reservation;
 
 import de.PogChampIsAlreadyTaken.Papaya.Webshop.Baseclasses.Reservation.Reservation;
 import de.PogChampIsAlreadyTaken.Papaya.Webshop.Baseclasses.Reservation.RestaurantTable;
+import org.jboss.logging.Logger;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.Date;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -17,48 +19,38 @@ import java.util.List;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class Reservationsystem {
+    private static final Logger LOG = Logger.getLogger(Reservationsystem.class);
 
     @POST
     @Transactional
     @Path("/tables")
     public Response postTable(RestaurantTable[] tables) {
-        for (int i = 0; i < tables.length; i++) {
-            tables[i].persist();
+        LOG.info("POSt new Tables: "+Arrays.toString(tables));
+
+        if(tables == null){
+            LOG.error("There is no table to be inserted.");
+            return Response.status(400).entity("There is no table to be inserted in this request").build();
         }
-        return Response.ok().entity(tables).build();
+        for (RestaurantTable table : tables) {
+            //check if table to be inserted already exist
+            if(RestaurantTable.findById(table.id) == null){
+                LOG.error("Table already exist. Table: "+table.toString());
+                return Response.status(409).entity("Table already exist. Table: "+table.toString()).build();
+            }
+            table.persist();
+        }
+
+        LOG.info("Tables successful created. New tables: "+Arrays.toString(tables));
+        return Response.ok().status(201).build();
     }
 
     @POST
     @Transactional
-    public Response postReservation(String data) {
-        /*
-        Reservation reservation = new Reservation();
-        reservation.name = name;
-        reservation.tableid = tableid;
-        reservation.phonenumber = phonenumber;
-        reservation.reservationsdate = new Date(reservationsdate);*/
-        //buissness logic
-        //only persist, if
-        /*
-        Reservierung:
-
-Tisch ist für 2 h reserviert.
-
-Möchte man reservieren, so gibt man den Start-Zeitpunkt ein.
-
-Die Startzeit wird genommen und es wird überprüft:
-
-Gibt es bereits den Tisch schon mit dieser Startzeit?
-Liegt der Tisch in einer 2h-Sperre?
-Wenn nicht --> Reservierung erfolgreich
-Sonst: Fehlerfall --> Tisch kann nicht reserviert werden, andere Zeit wählen
-2 unterschiedliche Restcalls:
-POST: CheckAvailability
-POSt: Reservation
-         */
-
+    @Path("/{time}")
+    public Response postReservation(@PathParam("time") Long time, Reservation reservation) {
+        //übersetze long in time
         //reservation.persist();
-        return Response.ok().entity(data).build();
+        return Response.ok().entity(reservation).build();
     }
 
     @Path("/table")
